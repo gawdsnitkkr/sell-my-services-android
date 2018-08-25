6c5874b3bdaaa9f6a36700580a2d25d3f7421ed1
varunon9 <varunon9@gmail.com>
2018-08-25 23:39:20 +0530

    added service details UI

--- app/src/main/java/me/varunon9/sellmyservices/servicefragments/ServiceDetailsFragment.java
+++ app/src/main/java/me/varunon9/sellmyservices/servicefragments/ServiceDetailsFragment.java
@@ -6,14 +6,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import me.varunon9.sellmyservices.R;
+import me.varunon9.sellmyservices.ServiceActivity;
+import me.varunon9.sellmyservices.db.models.Service;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ServiceDetailsFragment extends Fragment {
 
+    private ServiceActivity serviceActivity;
+    private String TAG = "ServiceDetailsFragment";
+    private TextView serviceNameTextView;
+    private TextView serviceDescriptionTextView;
+    private TextView serviceLocationTextView;
+    private TextView serviceTagsTextView;
+    private TextView serviceRatingTextView;
 
     public ServiceDetailsFragment() {
         // Required empty public constructor
@@ -24,7 +34,24 @@ public class ServiceDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_service_details, container, false);
+        View rootView =
+                inflater.inflate(R.layout.fragment_service_details, container, false);
+        serviceActivity = ((ServiceActivity) getActivity());
+        Service service = serviceActivity.service;
+
+        serviceNameTextView = rootView.findViewById(R.id.serviceNameTextView);
+        serviceRatingTextView = rootView.findViewById(R.id.serviceRatingTextView);
+        serviceDescriptionTextView = rootView.findViewById(R.id.serviceDescriptionTextView);
+        serviceTagsTextView = rootView.findViewById(R.id.serviceTagsTextView);
+        serviceLocationTextView = rootView.findViewById(R.id.serviceLocationTextView);
+
+        serviceNameTextView.setText(service.getName());
+        serviceDescriptionTextView.setText(service.getDescription());
+        serviceTagsTextView.setText(service.getTags());
+        serviceRatingTextView.setText(String.valueOf(service.getRating()));
+        serviceLocationTextView.setText(service.getLocation());
+
+        return rootView;
     }
 
 }
